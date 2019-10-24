936fd1bd3d575a1d08e3e60b46de0e9a41e15e37
KF <kevin.fagot@edu.itescia.fr>
2019-10-24 13:42:10 +0200

    change to model & add detailscomp

--- projectH/Components/StudentDetailsComponent.js
+++ projectH/Components/StudentDetailsComponent.js
@@ -1,10 +1,38 @@
-import {Component} from 'react-native';
+import {Component, StyleSheet, View} from 'react-native';
 
 class StudentDetailsComponent extends Component{
     
     constructor(props){
         super(props);
+        this.state = {
+            studentName: "",
+            studentFirstName: "",
+            studentImg: "",
+            studentGroup: "",
+            studentSkills: []
+        }
     }
 
+    render(){
+        return(
+
+            <View style={styles.view}>
+
+
+            </View>
+
+
+        )
+
+    }
+
+    const styles = StyleSheet.create({
+        view: {
+            flex: 1,
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+        },
+    });
 
 }
---
+++ projectH/Models/StudentModel.js
@@ -0,0 +1,18 @@
+import { createStore, applyMiddleware } from 'redux';
+import { combineForms } from 'react-redux-form';
+
+const initialStudent = {
+    id : 0,
+    name: '',
+    firstname: '',
+    img: '',
+    group: ''
+};
+
+
+const store = createStore(combineForms({
+    student: initialStudent
+}));
+
+export default store;
+
